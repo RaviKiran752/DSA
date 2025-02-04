5950e2d21c844e34b13789d0b552d39a200dbbb5
RaviKiran752 <krpkrpkrpkrp942@gmail.com>
2025-02-04 13:02:22 +0530

    Create singly_linked_list.py

---
+++ singly_linked_list.py
@@ -0,0 +1,31 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        if not self.head:
+            self.head = Node(data)
+            return
+        cur = self.head
+        while cur.next:
+            cur = cur.next
+        cur.next = Node(data)
+
+    def print_list(self):
+        cur = self.head
+        while cur:
+            print(cur.data, end=" -> ")
+            cur = cur.next
+        print("None")
+
+# Usage
+ll = LinkedList()
+ll.append(1)
+ll.append(2)
+ll.append(3)
+ll.print_list()  # Output: 1 -> 2 -> 3 -> None
